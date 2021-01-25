7a1570b9b875e3903d59939af34d22ae49c10b42
Thomas van den Bighelaar <thomas@beardiegames.com>
2021-01-25 01:43:45 +0100

    code simplefication && use of arrays to enforce use of stack

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,127 @@
-
+use std::fmt::Debug;
+use std::convert::TryInto;
 use std::time::{SystemTime};
-use std::cell::{RefCell, RefMut, Ref};
-use std::collections::VecDeque;
 
-type Pointer = usize;
+// enum Action { 
+//     Spawn(NameTag), 
+//     Destroy(ObjectIndex),
+//     AddComponent(ObjectIndex, NameTag), 
+//     RemoveComponent(ObjectIndex, NameTag),
+// }
 
-struct Pool<T> {
-    pool: Vec<T>,
-    active: Vec<Pointer>,
-    free: Vec<Pointer>,
-    iter: Pointer,
-}
+// struct Actions(Vec<Action>);
 
-impl<T> Pool<T> {
-    fn new<F: FnMut(usize) -> T>(size: usize, mut on_new: F) -> Self {
-        let mut pool = Vec::<T>::with_capacity(size);
-        let mut active = Vec::with_capacity(size);
-        let mut free = Vec::with_capacity(size);
-        
-        for i in 0..size {
-            pool.push(on_new(i));
-            free.push(i)
-        }
-        Pool { pool, active, free, iter: 0 }
-    }
+// impl Actions {
+//     fn new() -> Self { Self(Vec::new()) }
+    
+//     pub fn spawn(&mut self, name: &str) {
+//         self.0.push(Action::Spawn(NameTag::from_str(name)));
+//     }
 
-    fn spawn(&mut self) -> Option<Pointer> {
-        if let Some(pointer) = self.free.pop() {
-            self.active.push(pointer);
-            return Some(pointer);
-        }
-        None
-    }
+//     pub fn destroy(&mut self, target: usize) {
+//         self.0.push(Action::Destroy(target));
+//     }
 
-    fn destroy(&mut self, pointer: &Pointer) {
-        if let Some(i) = self.active.iter().position(|p| p == pointer) {
-            self.active.remove(i);
-            self.free.push(i);
-        }
-    }
+//     pub fn add_component(&mut self, target: usize, component_name: &str) {
+//         self.0.push(
+//             Action::AddComponent(target, NameTag::from_str(component_name))
+//         );
+//     }
 
-    pub fn borrow(&self, pointer: &Pointer) -> &T {
-        &self.pool[*pointer]
-    }
+//     pub fn remove_component(&mut self, target: usize, component_name: &str) {
+//         self.0.push(
+//             Action::RemoveComponent(target, NameTag::from_str(component_name))
+//         );
+//     }
 
-    pub fn edit(&mut self, pointer: &Pointer) -> &mut T {
-        &mut self.pool[*pointer]
-    }
+//     pub fn clear(&mut self) {
+//         self.0.clear();
+//     }
 
-    pub fn find<P>(&self, mut predicate: P) -> Option<Pointer>
-    where P: FnMut(&T) -> bool {
-        
-        for active in &self.active {
-            if predicate(&self.pool[*active]) {
-                return Some(*active);
-            }
-        }
-        None
-    }
+//     pub fn list(&self) -> &Vec<Action> {
+//         &self.0
+//     }
+// }
 
-    pub fn list(&self) -> &Vec<Pointer> {
-        &self.active
-    }
+const MAX_OBJECTS: usize = 10000;
 
-    pub fn use_count(&self) -> usize {
-        self.active.len()
-    }
+type Objects<T> = [T; MAX_OBJECTS];
+type Entities = [BitFlags; MAX_OBJECTS];
+type ObjectIndex = usize;
+type ComponentIndex = usize;
 
-    pub fn most_recent(&self) -> &Pointer {
-        &self.active[self.active.len()-1]
-    }
-}
-
-
-struct Ecs<T: Default> {
-    objects: Pool<T>, // object data pool, in other words entity component data
-    entities: Pool<Entity>, // object pool reference & implement component flags
-    components: Components, // component definitions, flag position & amount of components available
+struct Ecs<T: Default> { 
+    objects: Objects<T>, // object data pool, in other words entity component data
+    entities: Entities, // object component implementation flags
     systems: Vec<Box<dyn System<T>>>, // behaviour wrappers for executing custom behaviour scripts
+    components: Vec<Component>, // component definitions, flag position & amount of components available
+    //actions: Actions, // perform actions that can only be handled after the update sequence
+    active: Vec<ObjectIndex>,
+    free: Vec<ObjectIndex>,
 }
 
-impl<T: Default> Ecs<T> {
-    pub fn new(size: usize) -> Self {
-        let objects: Pool<T> = Pool::new(size, |i| { 
-            Default::default() 
-        });
-        let entities: Pool<Entity> = Pool::new(size, |i| { 
-            Entity::new(i, &format!("unused-{}", i)) 
-        });
+impl<T: Default + Debug> Ecs<T> {
+
+    fn new() -> Self {
+        let mut create_objects = Vec::<T>::with_capacity(MAX_OBJECTS);
+        create_objects.resize_with(MAX_OBJECTS, Default::default);
+
+        let mut create_entities = Vec::<BitFlags>::with_capacity(MAX_OBJECTS);
+        create_entities.resize_with(MAX_OBJECTS, Default::default);
+
+        let mut free = Vec::with_capacity(MAX_OBJECTS);
+        for i in 0..MAX_OBJECTS { free.push(i); }
 
         Ecs { 
-            objects, 
-            entities, 
-            components: Components::new(),
-            systems: Vec::new(), 
+            objects: create_objects.try_into().unwrap(),
+            entities: create_entities.try_into().unwrap(),
+            systems: Vec::new(),
+            components: Vec::new(),
+           // actions: Actions::new(),
+            active: Vec::new(),
+            free,
         }
     }
 
-    pub fn update(&mut self) {
+    fn update(&mut self) {
+        // update routine
         for system in &mut self.systems {
-            for entity in self.entities.list() {
-                let comp = &self.entities.borrow(entity).components;
-                if comp.0 == comp.0 & system.requires_components().0
+            for pointer in &self.active {
+                if self.entities[*pointer].0 == 
+                    self.entities[*pointer].0 & system.requires_components().0 
                 {
-                    system.update(self.entities.borrow(entity), &mut self.objects);
+                    system.update(&pointer, &mut self.objects, &self.active);//, &mut self.actions);
                 }
             }
         }
+
+        // handle actions
+        // for action in self.actions.list() {
+        //     match action {
+        //         Action::Spawn(s) => {
+        //             if self.objects.spawn().is_some() {
+        //                 if let Some(pointer) = self.entities.spawn() {
+        //                     self.entities.edit(&pointer).rename(s);
+        //                 }
+        //             }
+        //         }, 
+        //         Action::Destroy(p) => { 
+        //             self.objects.destroy(p);
+        //             self.entities.destroy(p);
+        //         }, 
+        //         Action::AddComponent(p, s) => {
+        //             if let Some(component) = self.components.get(s) {
+        //                 self.entities.edit(p).add_component(component.clone());
+        //             }
+        //         }, 
+        //         Action::RemoveComponent(p, s) => {
+        //             if let Some(component) = self.components.get(s) {
+        //                 self.entities.edit(p).remove_component(component.clone());
+        //             }
+        //         }, 
+        //     }
+        // }
+        // self.actions.clear();
     }
 
     pub fn define_system(&mut self, name: &str, system: Box<dyn System<T>>) {
@@ -116,51 +131,41 @@ impl<T: Default> Ecs<T> {
     }
 
     // setup new components by giving them a name and a flag position
-    pub fn define_component(&mut self, name: &str) -> &Component {
-        let num_components = self.components.0.len();
-        self.components.0.push(Component::new(num_components, name));
-        &self.components.0[num_components]
+    pub fn define_component(&mut self, name: &str) -> ComponentIndex {
+        let comp_index = self.components.len();
+        self.components.push(Component::new(comp_index.clone(), name));
+        comp_index
     }
 
-    pub fn add_component(&mut self, entity: &Entity, component: &Component) {
-        self.entities.edit(entity.pointer()).set_component(component.clone(), true);
+    pub fn get_component(&self, name: &str) -> Option<&Component> {
+        self.components.iter().find(|x| *x.name() == NameTag::from_str(name))
     }
 
-    pub fn remove_component(&mut self, entity: &Entity, component: &Component) {
-        self.entities.edit(entity.pointer()).set_component(component.clone(), false);
+    pub fn add_component(&mut self, target: &usize, component: &ComponentIndex) {
+        self.entities[*target].set_bit(*component, true);
     }
 
-    pub fn spawn(&mut self, name: &str) -> Option<Entity> {
-        if self.objects.spawn().is_some() {
-            if let Some(pointer) = self.entities.spawn() {
-                self.entities.edit(&pointer).rename(name);
-                return Some(self.entities.edit(&pointer).clone())
-            }
+    pub fn remove_component(&mut self, target: &usize, component: &ComponentIndex) {
+        self.entities[*target].set_bit(*component, false);
+    }
+
+    pub fn spawn(&mut self, name: &str) -> Option<usize> {
+        if let Some(pointer) = self.free.pop() {
+            self.active.push(pointer);
+            return Some(pointer);
         }
         None
     }
 
-    pub fn destroy(&mut self, entity: &Entity){
-        self.objects.destroy(entity.pointer());
-        self.entities.destroy(entity.pointer());
-    }
-
-    pub fn find(&mut self, name: &str) -> Option<Entity> {
-        self.entities
-            .find(|e| *e.name() == NameTag::from_str(name))
-            .map(|p| self.entities.borrow(&p).clone())
-    }
-
-    pub fn borrow(&mut self, entity: &Entity) -> &T {
-        self.objects.borrow(entity.pointer())
-    }
-
-    pub fn edit(&mut self, entity: &Entity) -> &mut T {
-        self.objects.edit(entity.pointer())
+    pub fn destroy(&mut self, target: &usize){
+        if let Some(i) = self.active.iter().position(|pointer| pointer == target) {
+            self.active.remove(i);
+            self.free.push(i);
+        }
     }
 }
 
-#[derive(Clone, Default, PartialEq)]
+#[derive(Default, Clone, PartialEq)]
 struct NameTag([u8; 16]);
 
 impl NameTag {
@@ -187,61 +192,24 @@ impl NameTag {
 }
 
 #[derive(Clone)]
-struct ObjectRef {
-    pointer: usize,
+struct Component {
+    index: usize,
     name: NameTag,
 }
 
-impl ObjectRef {
-    fn new(pointer: usize, name: &str) -> Self {
-        Self { pointer, name: NameTag::from_str(name) } 
+impl Component {
+    fn new(index: usize, name: &str) -> Self {
+        Component {
+            index,
+            name: NameTag::from_str(name),
+        }
     }
 
-    pub fn pointer(&self) -> &usize { &self.pointer }
+    pub fn index(&self) -> &usize { &self.index }
     pub fn name(&self) -> &NameTag { &self.name }
 
-    pub fn rename(&mut self, name: &str) {
-        self.name = NameTag::from_str(name)
-    }
-}
-
-
-#[derive(Clone)]
-struct Entity {
-    obj_ref: ObjectRef,
-    components: ComponentList,
-}
-impl Entity {
-    fn new(pointer: usize, name: &str) -> Self {
-        Self { 
-            obj_ref: ObjectRef::new(pointer, name), 
-            components: ComponentList::new() 
-        } 
-    }
-
-    pub fn pointer(&self) -> &usize { &self.obj_ref.pointer }
-    pub fn name(&self) -> &NameTag { &self.obj_ref.name }
-    pub fn rename(&mut self, name: &str) { self.obj_ref.rename(name); }
-
-    pub fn set_component(&mut self, component: Component, to: bool) { 
-        self.components.set_bit(*component.pointer(), to);
-    }
-}
-impl PartialEq for Entity {
-    fn eq(&self, other: &Entity) -> bool {
-        self.pointer() == other.pointer()
-    }
-}
-
-type Component = ObjectRef;
-
-struct Components(Vec<Component>);
-impl Components {
-    fn new() -> Self {
-        Self(Vec::with_capacity(32))
-    }
-    fn get(&self, name: &str) -> Option<&Component> {
-        self.0.iter().find(|x| *x.name() == NameTag::from_str(name))
+    pub fn compare_str(&self, name: &str) -> bool {
+        self.name == NameTag::from_str(name)
     }
 }
 
@@ -250,10 +218,7 @@ type ComponentList = BitFlags;
 impl ComponentList {
     fn new() -> Self { Self(0) }
     fn add(&mut self, component: Component) {
-        self.set_bit(*component.pointer(), true)
-    }
-    fn has(&self, other: &Self) -> bool {
-        other.0 == self.0 & other.0
+        self.set_bit(*component.index(), true)
     }
 }
 impl PartialEq for ComponentList {
@@ -262,13 +227,13 @@ impl PartialEq for ComponentList {
     }
 }
 
-#[derive(Clone, Default)]
+#[derive(Default, Debug)]
 struct BitFlags(u32);
 impl BitFlags {
-    pub fn set_bit(&mut self, position: usize, to: bool) {
+    pub fn set_bit(&mut self, at_index: usize, to: bool) {
         match to {
-            true => self.enable_bits(1 << position),
-            false => self.disable_bits(1 << position),
+            true => self.enable_bits(1 << at_index),
+            false => self.disable_bits(1 << at_index),
         }
     }
     pub fn enable_bits(&mut self, bits: u32) { self.0 |= bits }
@@ -276,9 +241,10 @@ impl BitFlags {
 }
 
 trait System<T: Default> {
-    fn init(&mut self, components: &Components);
+    fn init(&mut self, components: &Vec<Component>);
     fn requires_components(&self) -> &ComponentList;
-    fn update(&mut self, entity: &Entity, objects: &mut Pool<T>);
+    fn update(&mut self, target: &ObjectIndex, objects: &mut Objects<T>, active: &Vec<usize>);//, actions: &mut Actions);
+    //fn on_spawn_response(&mut self, spawn: &mut Entity, object: &mut T);
 }
 
 
@@ -286,7 +252,7 @@ trait System<T: Default> {
 /// 
 
 fn main() {
-    let mut ecs = Ecs::new(100);
+    let mut ecs = Ecs::new();
 
     let comp1 = ecs.define_component("call-1").clone();
     let comp2 = ecs.define_component("call-2").clone();
@@ -300,22 +266,13 @@ fn main() {
     ecs.add_component(&entity1, &comp1);
 
     let entity2 = ecs.spawn("entity-2").unwrap();
-    ecs.add_component(&entity2, &comp1);
+    ecs.add_component(&entity2, &comp2);
 
     let entity3 = ecs.spawn("entity-3").unwrap();
     ecs.add_component(&entity3, &comp3);
 
     let num_updates: u128 = 100_000_000;
     let num_calls: f64 = 3.0 * num_updates as f64;
-
-    // loop {
-    //     let now = SystemTime::now();
-    //     while now.elapsed().unwrap().as_nanos() < 1_000_000_000 {
-    //         ecs.update();
-    //     }
-    //     println!("updates {} calls/s", ecs.borrow(&entity1).call1.clone() * 3);
-    //     ecs.edit(&entity1).call1 = 0;
-    // }
 
     loop {
         let now = SystemTime::now();
@@ -324,17 +281,17 @@ fn main() {
 
         let elapsed_res = now.elapsed();
         match elapsed_res {
-            Ok(elapsed) => println!("updates {} calls/s", (1_000_000_000.0 / elapsed.as_nanos() as f64) * num_calls),
+            Ok(elapsed) => println!("updates {} M calls/s", (1_000.0 / elapsed.as_nanos() as f64) * num_calls),
             Err(e)      => println!("Error: {:?}", e),
         }
 
-        // println!(" - result 1: {}, {}, {}", ecs.borrow(&entity1).call1.clone(), ecs.borrow(&entity1).call2.clone(), ecs.borrow(&entity1).call3.clone());
-        // println!(" - result 2: {}, {}, {}", ecs.borrow(&entity2).call1.clone(), ecs.borrow(&entity2).call2.clone(), ecs.borrow(&entity2).call3.clone());
-        // println!(" - result 3: {}, {}, {}", ecs.borrow(&entity3).call1.clone(), ecs.borrow(&entity3).call2.clone(), ecs.borrow(&entity3).call3.clone());
+        // println!(" - result 1: {}, {}, {}", ecs.scene.borrow(&entity1).call1.clone(), ecs.scene.borrow(&entity1).call2.clone(), ecs.scene.borrow(&entity1).call3.clone());
+        // println!(" - result 2: {}, {}, {}", ecs.scene.borrow(&entity2).call1.clone(), ecs.scene.borrow(&entity2).call2.clone(), ecs.scene.borrow(&entity2).call3.clone());
+        // println!(" - result 3: {}, {}, {}", ecs.scene.borrow(&entity3).call1.clone(), ecs.scene.borrow(&entity3).call2.clone(), ecs.scene.borrow(&entity3).call3.clone());
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 struct Cell {
     pub call1: u128,
     pub call2: u128,
@@ -343,79 +300,80 @@ struct Cell {
 
 
 #[derive(Default)]
-struct Call1 {
-    requirements: ComponentList,
-}
+struct Call1 (ComponentList);
 
 impl Call1 {
-    pub fn new() -> Self {
-        Self { requirements: ComponentList::new() }
-    }
+    pub fn new() -> Self { Self (ComponentList::new()) }
 }
 
 impl System<Cell> for Call1 {
 
-    fn init(&mut self, components: &Components) {
-        self.requirements.add(components.get("call-1").unwrap().clone());
+    fn init(&mut self, components: &Vec<Component>) {
+        if let Some(c) = components.iter().find(|c| c.compare_str("call-1") ) {
+            self.0.add(c.clone());
+        }
     }
 
-    fn requires_components(&self) -> &ComponentList {
-        &self.requirements
+    fn requires_components(&self) -> &ComponentList { &self.0 }
+
+    fn update(&mut self, target: &ObjectIndex, objects: &mut Objects<Cell>, active: &Vec<usize>) {//, actions: &mut Actions) {
+        objects[*target].call1 += 1;
+        //actions.spawn("test");
     }
 
-    fn update(&mut self, entity: &Entity, objects: &mut Pool<Cell>) {
-        objects.edit(entity.pointer()).call1 += 1
-    }
+    // fn on_spawn_response(&mut self, spawn: &mut Entity, object: &mut Cell) {
+
+    // }
 }
 
 #[derive(Default)]
-struct Call2{
-    requirements: ComponentList,
-}
+struct Call2 (ComponentList);
 
 impl Call2 {
-    pub fn new() -> Self {
-        Self { requirements: ComponentList::new() }
-    }
+    pub fn new() -> Self { Self (ComponentList::new()) }
 }
 
 impl System<Cell> for Call2 {
 
-    fn init(&mut self, components: &Components) {
-        self.requirements.add(components.get("call-2").unwrap().clone());
+    fn init(&mut self, components: &Vec<Component>) {
+        if let Some(c) = components.iter().find(|c| c.compare_str("call-2") ) {
+            self.0.add(c.clone());
+        }
     }
 
-    fn requires_components(&self) -> &ComponentList {
-        &self.requirements
+    fn requires_components(&self) -> &ComponentList { &self.0 }
+
+    fn update(&mut self, target: &ObjectIndex, objects: &mut Objects<Cell>, active: &Vec<usize>){//, actions: &mut Actions) {
+        objects[*target].call2 += 1
     }
 
-    fn update(&mut self, entity: &Entity, objects: &mut Pool<Cell>) {
-        objects.edit(entity.pointer()).call2 += 1
-    }
+    // fn on_spawn_response(&mut self, spawn: &mut Entity, object: &mut Cell) {
+
+    // }
 }
 
 #[derive(Default)]
-struct Call3{
-    requirements: ComponentList,
-}
+struct Call3 (ComponentList);
 
 impl Call3 {
-    pub fn new() -> Self {
-        Self { requirements: ComponentList::new() }
-    }
+    pub fn new() -> Self { Self (ComponentList::new()) }
 }
 
 impl System<Cell> for Call3 {
 
-    fn init(&mut self, components: &Components) {
-        self.requirements.add(components.get("call-3").unwrap().clone());
+    fn init(&mut self, components: &Vec<Component>) {
+        if let Some(c) = components.iter().find(|c| c.compare_str("call-3") ) {
+            self.0.add(c.clone());
+        }
     }
 
-    fn requires_components(&self) -> &ComponentList {
-        &self.requirements
+    fn requires_components(&self) -> &ComponentList { &self.0 }
+
+    fn update(&mut self, target: &ObjectIndex, objects: &mut Objects<Cell>, active: &Vec<usize>){//, actions: &mut Actions) {
+        objects[*target].call3 += 1;
     }
 
-    fn update(&mut self, entity: &Entity, objects: &mut Pool<Cell>) {
-        objects.edit(entity.pointer()).call3 += 1
-    }
+    // fn on_spawn_response(&mut self, spawn: &mut Entity, object: &mut Cell) {
+
+    // }
 }
